8972e9a19c704802a6a842976ee44b55c8a92018
Asus <Asus@172.28.31.52>
2022-03-29 17:41:04 +0530

    second commit

--- SampleProject/src/samplePackage/testClass.java
+++ SampleProject/src/samplePackage/testClass.java
@@ -5,6 +5,8 @@ public class testClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Dineth");
+		System.out.println("Hello Ishma");
+		System.out.println("Hello Nisura");
 
 	}
 
